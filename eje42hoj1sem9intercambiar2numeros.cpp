2315a53fa426ffb5b74f5c243d3a389e847ac3ee
Cesar <30559667+CesarAroon@users.noreply.github.com>
2020-12-23 21:56:36 -0500

    Add files via upload

---
+++ eje42hoj1sem9intercambiar2numeros.cpp
@@ -0,0 +1,85 @@
+﻿// ConsoleApplication1.cpp : main project file.
+/*
+Implemente una función que permita intercambiar
+dos variables de tal forma que la primera variable siempre sea menor que la segunda.*/
+
+#include "stdafx.h"
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<iomanip>
+
+using namespace std;
+using namespace System;
+
+
+
+void intercambiar(int *a, int *b)
+{
+	if (*a > *b)
+	{
+		int *temp;
+		temp= a;
+		a = b;
+		temp=b;
+	}
+	
+		cout << "el valor de x es: " << *b << endl;
+		cout << "el valor de y es: " << *a << endl;
+	
+
+}
+
+
+
+int main()
+{
+	int x, y;
+	int *px, *py;//declaracion de variables punteros que almacenaran direcciones de memoria
+	px = &x; py = &y;//asignacion de direcciones de memoria a estas variables
+
+	cout << "ingrese valor : "; cin >> x;
+	cout << "ingrese valor : "; cin >> y;
+	intercambiar(px, py);
+	_getch();
+}
+
+
+//
+//#include "stdafx.h"
+//#include<iostream>
+//#include<conio.h>
+//#include<stdlib.h>
+//#include<math.h>
+//#include<iomanip>
+//
+//using namespace std;
+//using namespace System;
+//
+//
+//
+//void intercambiar(int a, int b)
+//{
+//	if (a > b)
+//	{
+//		int temp = a;
+//		a = b;
+//		b = temp;
+//	}
+//	cout << "el valor de x es: " << a << endl;
+//	cout << "el valor de y es: " << b << endl;
+//
+//
+//}
+//
+//
+//
+//int main()
+//{
+//	int x, y;
+//	cout << "ingrese valor x : "; cin >> x;
+//	cout << "ingrese valor y : "; cin >> y;
+//	intercambiar(x, y);
+//	_getch();
+//}
